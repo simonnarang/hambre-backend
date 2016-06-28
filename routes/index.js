45c9ba95ba550532adec135910d015617848c1c2
Simon Narang <simonarang202@gmail.com>
2016-06-27 21:53:28 -0400

    Hambreaoiwdnoawndlkj
    such abhor

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var Yelp = require('yelp');
+var router = express.Router();
+
+var yelp = new Yelp({
+  consumer_key: 'T8p3rlxD_eQLz6Jl7ePghw',
+  consumer_secret: 'q_y5_QnNCzfe2MiSyB7baEqGkCk',
+  token: 'GDHL7uPryiq5DNf1nScszTgyH6Uba7Zg',
+  token_secret: 'LzHSrY-YdxEcRqWpmUVHkg5ZcfI'
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  yelp.search({ term: 'food', location: 'Rochester' })
+      .then(function (data) {
+        console.log(data);
+        res.send(data)
+        //res.render()
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
+});
+
+module.exports = router;
